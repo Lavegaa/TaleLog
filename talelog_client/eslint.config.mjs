f1e6b79b7a13940946a28770b003e238ad44f1d8
lavegaa <cksgh405@naver.com>
2025-01-13 13:44:59 +0900

    ⚡️ : add eslint

--- talelog_client/eslint.config.mjs
+++ talelog_client/eslint.config.mjs
@@ -10,7 +10,38 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      ".next/**",
+      ".next/*",
+      "next-env.d.ts",
+      "node_modules/**"
+    ]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "indent": ["error", 2],
+      "func-style": ["error", "declaration"],
+      "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+      "semi": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-trailing-spaces": "error",
+      "space-before-blocks": "error",
+      "space-before-function-paren": ["error", {
+        "anonymous": "always",
+        "named": "never",
+        "asyncArrow": "always"
+      }],
+      "space-in-parens": ["error", "never"],
+      "space-infix-ops": "error",
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "arrow-spacing": "error"
+    }
+  }
 ];
 
 export default eslintConfig;
